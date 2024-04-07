6bc6cf787a382abc559097703cc5228a750b4260
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-04-07 12:19:22 +0530

    init backend

--- backend/application/mail_service.py
+++ backend/application/mail_service.py
@@ -1,13 +1,19 @@
-from smptlib import SMTP
-from email.mime.text import MIMEText
+from smtplib import SMTP
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-def send_email(email, subject):
+SMTP_HOST = "localhost"
+SMTP_PORT = 1025
+SENDER_EMAIL = 'narendr@study.iitm.ac.in'
+SENDER_PASSWORD = ''
+
+
+def send_message(to, subject, content_body):
     msg = MIMEMultipart()
-    msg['From'] = 'pallavipandey181@gmail.com'
-    msg['To'] = email
-    msg['Subject'] = subject
-    message = 'Your daily reminder'
-    msg.attach(MIMEText(message))
-    client = SMTP('smtp.gmail.com', 587)    
-    
+    msg["To"] = to
+    msg["Subject"] = subject
+    msg["From"] = SENDER_EMAIL
+    msg.attach(MIMEText(content_body, 'html'))
+    client = SMTP(host=SMTP_HOST, port=SMTP_PORT)
+    client.send_message(msg=msg)
+    client.quit()
